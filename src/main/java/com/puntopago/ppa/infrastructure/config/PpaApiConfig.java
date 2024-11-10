fd134381692d81bbd48827dd090230fa9162ade2
User <juan.torres@konexinnovation.com.co>
2024-11-10 12:44:36 -0500

    Title: adjust config. Description: adjusting api config

--- src/main/java/com/puntopago/ppa/infrastructure/config/PpaApiConfig.java
+++ src/main/java/com/puntopago/ppa/infrastructure/config/PpaApiConfig.java
@@ -12,22 +12,18 @@ import java.util.List;
 @Configuration
 public class PpaApiConfig {
 
-    @Value("${spring.config.activate.on-profile}")
-    private String profile;
-
-    private static final String SERVER_PORT = "2208";
+    @Value("${domain}")
+    private String domain;
 
     @Bean
     public OpenAPI springAccommodationOpenAPI() {
-        String prefix = profile.equals("dev") ? "http://localhost:" + SERVER_PORT : "https://ppa-production.up.railway.app";
-
         return new OpenAPI()
                 .info(new Info().title("PPA management API")
                         .description("API that provides services for the management, administration and control of Punto Pago Air (PPA)")
                         .version("v0.0.1"))
                 .servers(
                         List.of(
-                                new Server().url(prefix + "/api/v1/ppa").description("API Gateway")
+                                new Server().url(domain + "/api/v1/ppa").description("API Gateway")
                         )
                 );
     }
