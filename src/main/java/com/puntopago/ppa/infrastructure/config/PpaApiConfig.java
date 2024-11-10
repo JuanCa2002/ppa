9e51d31b310d642554338459ad510b6bdba381d6
User <juan.torres@konexinnovation.com.co>
2024-11-10 11:12:54 -0500

    Title: configuring api swagger. Description: configuring api swagger

--- src/main/java/com/puntopago/ppa/infrastructure/config/PpaApiConfig.java
+++ src/main/java/com/puntopago/ppa/infrastructure/config/PpaApiConfig.java
@@ -3,6 +3,7 @@ package com.puntopago.ppa.infrastructure.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,17 +12,22 @@ import java.util.List;
 @Configuration
 public class PpaApiConfig {
 
+    @Value("${spring.config.activate.on-profile}")
+    private String profile;
+
     private static final String SERVER_PORT = "2208";
 
     @Bean
     public OpenAPI springAccommodationOpenAPI() {
+        String prefix = profile.equals("dev") ? "http://localhost:" + SERVER_PORT : "https://ppa-production.up.railway.app";
+
         return new OpenAPI()
                 .info(new Info().title("PPA management API")
                         .description("API that provides services for the management, administration and control of Punto Pago Air (PPA)")
                         .version("v0.0.1"))
                 .servers(
                         List.of(
-                                new Server().url("http://localhost:" + SERVER_PORT +"/api/v1/ppa").description("API Gateway")
+                                new Server().url(prefix + "/api/v1/ppa").description("API Gateway")
                         )
                 );
     }
